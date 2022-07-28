531bf25118c9304e4ab5573f75e7838c330e9cc8
nishantk3686 <nishantkumar3686@gmail.com>
2022-07-28 20:41:26 +0530

    Lab Day

--- 28 July 2022/transposeMatrix.java
+++ 28 July 2022/transposeMatrix.java
@@ -1,49 +1,49 @@
 /*Program to get transpose of a matrix
-   By Nishant Kumar
-*/
+ by Nishant Kumar
+ */
 import java.util.Scanner;
 class transposeMatrix
 {
 public static void main(String args[])
 {
 	int i,j;
-	Scanner sc = new Scanner(System.in);
+	Scanner br = new Scanner(System.in);
 	System.out.println("Enter the rows for your matrix.");
-	int row = sc.nextInt();			//Input for rows of Matrix.
-	System.out.println("Enter the columns of your matrix");
-	int col = sc.nextInt();		//Input for columns of matrix
+	int row = br.nextInt();			//Input for rows of Matrix.
+	System.out.println("Enter the columns for your matrix.");
+	int col = br.nextInt();		//Input for columns of matrix.
 	int arr[][] = new int[row][col];
-	System.out.println("--Enter the elements for matrix--");
+	System.out.println("----Enter the elements for your matrix----");
 	for(i=0;i<row;i++)
 	{  
 		for(j=0;j<col;j++)
 		{
-			arr[i][j] = sc.nextInt();			//Input of elements of matrix
+			arr[i][j] = br.nextInt();			//input for elements of matrix
 		}
 	}
-	System.out.println("Given matrix elements ");
+	System.out.println("Matrix elements entered by you are");
 	for(i=0;i<row;i++)
 	{
 		for(j=0;j<col;j++)	
 		{
-			System.out.print(arr[i][j]+" ");	//Printing the elements of matrix
+			System.out.print(arr[i][j]+" ");	//Printing the elements of matrix 
 		}
-		System.out.println(" ");			//Printing space 
+		System.out.println(" ");			//Adding some space 
 	}
 	changeArray(row,col,arr);
-	sc.close();				//Scanner object closed.
+	br.close();				//Scanner object closed.
 }
 public static void changeArray(int len,int size,int brr[][])
 {
 	int m,n;
-	System.out.println("---Result of Transpose of your matrix----");
+	System.out.println("----Here is the Transpose of your matrix----");
 	for(m=0;m<len;m++)
 	{
 		for(n=0;n<size;n++)
 		{
-			System.out.print(brr[n][m]+" ");			//Doing Transpose of entered matrix.
+			System.out.print(brr[n][m]+" ");			//Performing Transpose of entered matrix.
 		}
-		System.out.println(" ");					//Printing space 
-}
+		System.out.println(" ");					//Adding space 
+	}
 }
 }
